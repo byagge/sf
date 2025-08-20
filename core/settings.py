0e2fd8d11f5e05dd2b443e811f0449f0e7061dbc
byagge <byagge@outlook.com>
2025-08-20 19:39:21 +0600

    allowed

--- core/settings.py
+++ core/settings.py
@@ -142,10 +142,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # Глобальная папка для статики
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# STATIC
+STATIC_URL = "/static/"                     # URL для статики
+STATICFILES_DIRS = [BASE_DIR / "static"]    # папка(и) с исходной статикой (dev/исходники)
+STATIC_ROOT = BASE_DIR / "staticfiles"      # куда collectstatic будет класть готовую статику
+
+# MEDIA
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
